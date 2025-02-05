8eb0fa475767265ff6496e18226beb7864062a28
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-02-05 10:38:02 +0530

    updated delete update in web

--- myjs/datadisplay.js
+++ myjs/datadisplay.js
@@ -1,39 +1,111 @@
+
+
+
+function myDel(id) {
+  let api = `http://localhost:3000/flights/${id}`;
+  fetch(api, { method: "DELETE" }).then((res) => {
+    alert("Record Deleted!!!");
+  });
+}
+
+function saveRow(id) {
+  let myemp = document.getElementById(`input1-${id}`);
+  let myName = document.getElementById(`input2-${id}`);
+  let mycity = document.getElementById(`input3-${id}`);
+  let mysalary = document.getElementById(`input4-${id}`);
+
+  // ✅ Check if elements exist before accessing value
+  if (!myemp || !myName || !mycity || !mysalary) {
+    alert("Error: Some input fields are missing!");
+    return;
+  }
+
+  let url = `http://localhost:3000/flights/${id}`;
+  fetch(url, {
+    method: "PUT",
+    body: JSON.stringify({
+      from: myemp.value,
+      to: myName.value,
+      departure: mycity.value,
+      return: mysalary.value,
+    }),
+    headers: {
+      "Content-type": "application/json;charset=utf-8",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Data updated successfully");
+        dataShow(); 
+      } else {
+        throw new Error("Error while updating");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
+function editRow(id) {
+ 
+  let inputs = document.querySelectorAll(
+    `#input1-${id}, #input2-${id}, #input3-${id}, #input4-${id}`
+  );
+
+ 
+  inputs.forEach((input) => {
+    input.removeAttribute("readonly");
+    input.style.border = "1px solid black"; 
+  });
+
+  
+  document.getElementById(`edit-${id}`).style.display = "none";
+  document.getElementById(`save-${id}`).style.display = "inline";
+}
+
+
+
 async function dataShow() {
   const api = "http://localhost:3000/flights";
   let table = `<table border="1" width="100%" align="center">
                     <tr>
-                        
-                      
                         <th>From</th>
                         <th>To</th>
                         <th>Departure</th>
                         <th>Return</th>
-                        
-                      
-                        
-
+                        <th>Actions</th>
                     </tr>`;
 
   const response = await fetch(api);
-
   const data = await response.json();
-  // console.log(data);
 
   data.map((item) => {
     table += `<tr>
-                        
-                        <td align="center">${item.from}</td>
-                        <td align="center">${item.to}</td>
-                        <td align="center">${item.departure}</td>
-                        <td align="center">${item.return}</td>
-                       
-                       
-                      </tr>`;
+                <td align="center">
+                  <input type="text" id="input1-${item.id}" value="${item.from}" readonly>
+                </td>
+                <td align="center">
+                  <input type="text" id="input2-${item.id}" value="${item.to}" readonly>
+                </td>
+                <td align="center">
+                  <input type="text" id="input3-${item.id}" value="${item.departure}" readonly>
+                </td>
+                <td align="center">
+                  <input type="text" id="input4-${item.id}" value="${item.return}" readonly>
+                </td>
+                <td align="center">
+                  <button onclick="editRow('${item.id}')" id="edit-${item.id}" class="button button-edit">Edit</button>
+                  <button onclick="saveRow('${item.id}')" id="save-${item.id}" class="button button-save" style="display:none">Save</button>
+                  <button onclick="myDel(${item.id})">Delete</button>
+                </td>
+              </tr>`;
   });
 
   table += "</table>";
   document.getElementById("demo").innerHTML = table;
 }
+
 
 
 
@@ -72,3 +144,4 @@ async function mySearch() {
   TAB += "</table>";
   document.getElementById("demo").innerHTML = TAB;
 }
+
